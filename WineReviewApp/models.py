cfa9948111e6eb3bbe89fc011ff8b0227cc5350a
chiranjev <koulsahil04321@gmail.com>
2019-01-14 23:37:48 +0530

    recommendation system using k means

--- WineReviewApp/models.py
+++ WineReviewApp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 import numpy as np
+from django.contrib.auth.models import User
+from django.db.models import Avg
+
 
 class Wine(models.Model):
     name = models.CharField(max_length=200)
     
-    # def average_rating(self):
-    #     all_ratings = map(lambda x: x.rating, self.review_set.all())
-    #     return np.mean(all_ratings)
+    def average_rating(self):
+        all_ratings = map(lambda x: x.rating, self.review_set.all())
+        return self.review_set.aggregate(Avg('rating'))['rating__avg']
+        # return np.mean(all_ratings)
         
     def __unicode__(self):
         return self.name
@@ -25,3 +29,10 @@ class Review(models.Model):
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
     rating = models.IntegerField(choices=RATING_CHOICES)
+
+class Cluster(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+
+    def get_members(self):
+        return "\n".join([u.username for u in self.users.all()])
